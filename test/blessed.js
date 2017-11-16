fdd8a199fbb201faaf77ad2f71f1b540ed1b28e9
bobiscool <tomisugly@163.com>
2017-11-16 14:22:18 +0800

    [Chg] seperate logo and logoText

--- test/blessed.js
+++ test/blessed.js
@@ -1,7 +1,7 @@
-var blessed = require('blessed');
-var chalk = require('chalk');
+var blessed = require("blessed");
+var chalk = require("chalk");
 var screen = blessed.screen({
-   smartCSR:true
+  smartCSR: true
 });
 var logo = `  
 ..@@@@@@@@@@@@@@@@@@@@@@@@@@@.. 
@@ -20,82 +20,86 @@ var logo = `
 ...@@@@@@@@@@@@@@@@@@@@@@@@@@.. `;
 
 function addChalk(text) {
-    let temText = "";
-    let textNotat = "";
-    for (var i in text) {
-      if (text[i] != "@") {
-        temText += `${i==0?'':'+'}chalk.white('${text[i]}')`;
-      }
-      if(text[i] == "@"){
-          temText +=`${i==0?'':'+'}'@'`
-      }
+  let temText = "";
+  let textNotat = "";
+  for (var i in text) {
+    if (text[i] != "@") {
+      temText += `${i == 0 ? "" : "+"}chalk.white('${text[i]}')`;
     }
-     temText = 'chalk.red('+temText+')'
-    return temText;
+    if (text[i] == "@") {
+      temText += `${i == 0 ? "" : "+"}'@'`;
+    }
   }
-  
-  function genLogo() {
-    var logoTextArray = logo.split("\n"); 
-    var logoTextArrayWrapChalk = []
+  temText = "chalk.red(" + temText + ")";
+  return temText;
+}
+
+function genLogo() {
+  var logoTextArray = logo.split("\n");
+  var logoTextArrayWrapChalk = [];
   logoTextArray.forEach(function(item, index) {
     logoTextArrayWrapChalk.push(addChalk(item));
   });
-//   console.log(title)
-   return logoTextArrayWrapChalk.join("+'\\n'+");
-  }
-  
+  //   console.log(title)
+  return logoTextArrayWrapChalk.join("+'\\n'+");
+}
+
 // console.log(eval(genLogo()));
 
 screen.title = "NetEase";
 
 var logoBox = blessed.box({
-   top:'center',
-   left:"center",
-   width:'280',
-   height:'480',
-   content:eval(genLogo()),
-   tags:true,
-   style:{
-       fg:'red',
-       bg:'black',
-       hover:{
-           bg:'green'
-       }
-   }
+  top: "center",
+  left: "center",
+  width: 33.5,
+  height: 18,
+  content: eval(genLogo()),
+  tags: true,
+  style: {
+    fg: "red",
+    bg: "black",
+
+    border: {
+      fg: "#f0f0f0"
+    },
+    hover: {
+      bg: "green"
+    }
+  }
 });
 
-
+var textBox = blessed.box({
+    top: "center",
+    left: "center",
+    width: 33.5,
+    height: 18,
+    content: eval(genLogo()),
+})
 var box = blessed.box({
-    top:'center',
-    left:"center",
-    width:'100%',
-    height:'100%',
-    content:'',
-    tags:true,
-    border:{
-        type:'line'
+  top: "center",
+  left: "center",
+  width: "100%",
+  height: "100%",
+  content: "",
+  tags: true,
+  border: {
+    type: "line"
+  },
+  style: {
+    fg: "red",
+    bg: "black",
+    border: {
+      fg: "#f0f0f0"
     },
-    style:{
-        fg:'red',
-        bg:'black',
-        border:{
-            fg:'#f0f0f0'
-        },
-        hover:{
-            bg:'green'
-        }
+    hover: {
+      bg: "green"
     }
- });
+  }
+});
 
-
-
-
-
-
-
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-    return process.exit(0);
-  });
+screen.key(["escape", "q", "C-c"], function(ch, key) {
+  return process.exit(0);
+});
 
 screen.append(box);
 screen.append(logoBox);
